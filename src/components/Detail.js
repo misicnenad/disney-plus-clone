7d64f1f6ddd15876b90cb03d3be45279407754e0
shonmisic <n.misic@outlook.com>
2021-11-02 19:16:03 +0100

    Fix console warnings

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -22,7 +22,7 @@ function Detail() {
                 setShouldRedirect(true)
             }
         })
-    })
+    }, [id])
 
     if (shouldRedirect) {
         return <Redirect to='/' />
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { auth, provider } from '../firebase'
 import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components'
 import { selectUserName, selectUserPhoto, setUserLogin, setSignOut } from '../features/user/userSlice'
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 function Header() {
     const dispatch = useDispatch()
@@ -55,30 +55,30 @@ function Header() {
             ) : (
                 <>
                     <NavMenu>
-                        <a>
+                        <StyledLink to={`/`}>
                             <img src='/images/home-icon.svg' alt='home' />
                             <span>HOME</span>
-                        </a>
-                        <a>
+                        </StyledLink>
+                        <StyledLink to={`/`}>
                             <img src='/images/search-icon.svg' alt='search' />
                             <span>SEARCH</span>
-                        </a>
-                        <a>
+                        </StyledLink>
+                        <StyledLink to={`/`}>
                             <img src='/images/watchlist-icon.svg' alt='watchlist' />
                             <span>WATCHLIST</span>
-                        </a>
-                        <a>
+                        </StyledLink>
+                        <StyledLink to={`/`}>
                             <img src='/images/original-icon.svg' alt='original' />
                             <span>ORIGINALS</span>
-                        </a>
-                        <a>
+                        </StyledLink>
+                        <StyledLink to={`/`}>
                             <img src='/images/movie-icon.svg' alt='movie' />
                             <span>MOVIES</span>
-                        </a>
-                        <a>
+                        </StyledLink>
+                        <StyledLink to={`/`}>
                             <img src='/images/series-icon.svg' alt='' />
                             <span>SERIES</span>
-                        </a>
+                        </StyledLink>
                     </NavMenu>
                     <UserImg src={userPhoto} alt='profile image' onClick={signUserOut} />
                 </>
@@ -102,49 +102,50 @@ const Logo = styled.img`
     width: 80px;
 `
 
+const StyledLink = styled(Link)`
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+    color: #f9f9f9;
+    text-decoration: none;
+
+    img {
+        height: 20px;
+    }
+
+    span {
+        font-size: 13px;
+        letter-spacing: 1.42px;
+        position: relative;
+
+        &:after {
+            content: '';
+            height: 2px;
+            background: white;
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: -6px;
+            opacity: 0;
+            transform-origin: left center;
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+            transform: scaleX(0);
+        }
+    }
+
+    &:hover {
+        span:after {
+            transform: scaleX(1);
+            opacity: 1;
+        }
+    }
+`
 const NavMenu = styled.div`
     display: flex;
     flex: 1;
     margin-left: 25px;
     align-items: center;
-
-    a {
-        display: flex;
-        align-items: center;
-        padding: 0 12px;
-        cursor: pointer;
-
-        img {
-            height: 20px;
-        }
-
-        span {
-            font-size: 13px;
-            letter-spacing: 1.42px;
-            position: relative;
-
-            &:after {
-                content: '';
-                height: 2px;
-                background: white;
-                position: absolute;
-                left: 0;
-                right: 0;
-                bottom: -6px;
-                opacity: 0;
-                transform-origin: left center;
-                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-                transform: scaleX(0);
-            }
-        }
-
-        &:hover {
-            span:after {
-                transform: scaleX(1);
-                opacity: 1;
-            }
-        }
-    }
 `
 const UserImg = styled.img`
     width: 48px;
