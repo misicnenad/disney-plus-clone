bd4eb361ff33013c6889571cb28a6f60867a1836
shonmisic <n.misic@outlook.com>
2021-11-04 12:12:21 +0100

    Add more bottom padding to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,7 +34,7 @@ export default Home
 
 const Container = styled.main`
     min-height: calc(100vh - 70px);
-    padding: 0 calc(3.5vw + 5px);
+    padding: 0 calc(3.5vw + 5px) calc(3.5vh + 5px);
     position: relative;
     overflow-x: hidden;
 
